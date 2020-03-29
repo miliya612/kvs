c012d74609feea460549628b872e9cdf92872712
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-03-29 18:25:27 +0900

    implement set subcommand

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -8,6 +8,7 @@ fn main() {
 
     match matches.subcommand() {
         ("get", Some(_)) => panic!("unimplemented"),
+        ("set", Some(_)) => panic!("unimplemented"),
         ("", None) => panic!("No subcommand was specified!"),
         _ => unreachable!(),
     }
