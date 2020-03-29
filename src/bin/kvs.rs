baee194ec3cb3cb0c5ec78218056d37cff33d4e0
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-03-29 17:28:31 +0900

    add no arg handling

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -4,5 +4,10 @@ use clap::App;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let _ = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml).get_matches();
+
+    if let Some(_) = matches.subcommand_name() {
+    } else {
+        panic!("No subcommand was used")
+    }
 }
