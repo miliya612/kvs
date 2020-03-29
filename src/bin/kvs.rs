5663e5f84bcf65837d8665c345fea8065fad68c5
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-03-29 19:03:38 +0900

    remove duplicated code for setting package info

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -2,9 +2,16 @@
 extern crate clap;
 use clap::App;
 
+use std::env;
+
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = App::from_yaml(yaml)
+        .name(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .get_matches();
 
     match matches.subcommand() {
         ("get", Some(_)) => panic!("unimplemented"),
