5b69f6eb71cb7ee701a241dfdaf827a96cf9aad2
Tomoki Ninomiya <t.miliya612@gmail.com>
2020-03-29 19:22:51 +0900

    implement get

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@ impl KvStore {
     pub fn set(&mut self, key: String, value: String) {
         self.store.insert(key, value);
     }
-    pub fn get(&self, _key: String) -> Option<String> {
-        panic!();
+    pub fn get(&self, key: String) -> Option<String> {
+        self.store.get(&key).cloned()
     }
     pub fn remove(&mut self, _key: String) {
         panic!();
